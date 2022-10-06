ae68d798732214d6d8efb817844069bd540da7de
Stephanie L <62116187+slow312@users.noreply.github.com>
2022-10-06 09:51:15 -0400

    first commit

---
+++ file1.py
@@ -0,0 +1,22 @@
+import json
+import requests
+import os
+import sys
+import time
+import crontab
+import pandas
+
+# getting JSON file and saving it 
+df = pandas.read_json('https://data.nasa.gov/resource/y77d-th95.json')
+
+# Save df to as local csv file
+df.to_csv('crontab/data/data.csv')
+
+# create a new file in the current working directory
+cwd = os.getcwd()
+print(cwd)
+now = time.time()
+nowStr = time.strftime("%Y-%m-%d_%H:%M:%S", time.localtime(now))
+
+with open(cwd + '/data/file1_' + nowStr + '.txt', 'w') as f:
+    f.write(str(df))
